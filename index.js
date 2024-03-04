a78b6dbc409a8c7e0730db5d8f7e9e408db7b46e
devgirlpro <agalidari@gmail.com>
2024-03-04 16:11:58 +0100

    jason file

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // const batman = require('./batman');
 // const superman = require('./superman')
 
-const math = require('./math');
+// const math = require('./math');
 
-const { add, subtract } = math;
+// const { add, subtract } = math;
 
 // console.log(math.add(2, 3));
 // console.log(math.subtract(17, 5));
 
-console.log(add(2, 3))
-console.log(subtract(17, 5));
+// console.log(add(2, 3))
+// console.log(subtract(17, 5));
+
+const data = require('./data.json');
+console.log(data)
