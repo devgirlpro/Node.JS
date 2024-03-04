54c9e099373c35c3ddd0d5a0e037fa6dbc17bd63
devgirlpro <agalidari@gmail.com>
2024-03-04 13:58:36 +0100

    dealing with object import and destructurig

--- iife.js
+++ iife.js
@@ -1,9 +1,14 @@
-(function () {
+(function (message) {
     const superHero = 'Batman';
-    console.log(superHero)
-})();
+    console.log(message, superHero)
+})('Hello');
 
-(function () {
+(function (message) {
     const superHero = 'Superman';
-    console.log(superHero)
-})();
+    console.log(message, superHero)
+})('Goodbye');
+
+//every module in node.js gets wrapped in an IIFE before being loaded
+
+// IIFE helps keep top-level variables scoped to the module rather than 
+// the gloal object
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const batman = require('./batman');
-const superman = require('./superman')
+// const batman = require('./batman');
+// const superman = require('./superman')
+
+const math = require('./math');
+
+console.log(math.add(2, 3));
+console.log(math.subtract(17, 5));
