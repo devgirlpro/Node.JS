07af83680e77591e310cf2cea8dc43964f834fc0
devgirlpro <agalidari@gmail.com>
2024-03-04 13:34:43 +0100

    undrestanding module scope

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
- const add = require("./add");
-
-console.log("Hi from index.js");
-
-const sum1 = add(5, 3)
-const sum2 = add(5, 5)
-console.log("sum1 from index.js", sum1)
-console.log("sum2 from index.js", sum2)
+const batman = require('./batman');
+const superman = require('./superman')
