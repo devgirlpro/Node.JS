5d52788bcd6f57f05e122f45368c602e5694f5f4
devgirlpro <agalidari@gmail.com>
2024-03-05 14:00:22 +0100

    stream & buffer

--- built-in-eventEmitter.js
+++ built-in-eventEmitter.js
@@ -4,14 +4,23 @@ const emitter = new EventEmitter();
 
 
 //on method to ansver the event 
-// emitter.on('order-Pizza', () => {
+// emitter.on('order-pizza', () => {
 //     console.log(`Order recived! Backing a Pizza`);
 // })
 
-//emitter.emit("order-Pizza")
+//emitter.emit("order-pizza")
 
 
-emitter.on('order-Pizza', (size, topping) => {
+emitter.on('order-pizza', (size, topping) => {
     console.log(`Order recived! Backing a  ${size} Pizza with a ${topping}`);
-})
-emitter.emit("order-Pizza", "large", "mushroom");
+});
+
+emitter.on('order-pizza', (size) => {
+    if(size === "large") {
+        console.log('Serving complimentary drink');
+    }
+});
+
+
+console.log("Do work before event occurs in the system")
+emitter.emit("order-pizza", "large", "mushroom");
